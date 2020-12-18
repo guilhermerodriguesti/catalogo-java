bfe20ada3009b7b8b8b520ecb66e37ca080e5050
Guilherme Rodrigues <811215-guilhermerodrigues@users.noreply.gitlab.com>
2020-12-18 07:47:32 -0300

    Update script install jenkins

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jdk -y
 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 vim /etc/default/jenkins
 
